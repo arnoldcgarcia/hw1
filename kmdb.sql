796b67c9baf0b21ef03e93dfad0fa3ef77f07404
arnoldcgarcia <arnold.garcia@kellogg.northwestern.edu>
2022-04-19 00:50:47 +0000

    HW 1 Edits

--- kmdb.sql
+++ kmdb.sql
@@ -98,6 +98,7 @@
 -- Turns column mode on but headers off
 .mode column
 .headers off
+.width 25 25 25
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 DROP TABLE IF EXISTS movies;
